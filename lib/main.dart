be2afa4a7bae4fc7109555e3a3d3d79050a3cc34
OwfukingG <yogiaryagunar@gmail.com>
2025-03-13 12:44:39 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const TemperatureConverterPage(),
+      home: const MyLoginPage(),
       debugShowCheckedModeBanner: false,
     );
   }
